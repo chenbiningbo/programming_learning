2239fd427891a257ac507997ad6855f1a0f52785
陈笔 <bi.chen@chnenergy.com.cn>
2020-02-13 20:17:09 +0800

    for loop use test

---
+++ test/for_loop/challenge.py
@@ -0,0 +1,6 @@
+numbers = [5, 2, 5, 2, 2]
+for x_counts in numbers:
+    output = ""
+    for count in range(x_counts):
+        output += "X"
+    print(output)
---
+++ test/for_loop/find_max_num.py
@@ -0,0 +1,6 @@
+numbers = [3, 5, 2, 7, 10, 8, 7]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
