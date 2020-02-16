e77086798897c73473b752f170298030f5baec2b
陈笔 <bi.chen@chnenergy.com.cn>
2020-02-16 14:42:07 +0800

    change another write way

--- test/for_loop/find_max_num.py
+++ test/for_loop/find_max_num.py
@@ -1,6 +1,9 @@
+def find_max(numbers):
+    max = numbers[0]
+    for number in numbers:
+        if number > max:
+            max = number
+    return max
+
 numbers = [3, 5, 2, 7, 10, 8, 7]
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
-print(max)
+print(find_max(numbers))
